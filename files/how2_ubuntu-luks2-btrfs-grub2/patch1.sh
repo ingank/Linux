f7cd13931daec00ab09de5c458e93c49d78c5801
Ingolf Ankert <dev@ingank.de>
2021-11-23 18:49:54 +0100

    Update patch1.sh

--- files/how2_ubuntu-luks2-btrfs-grub2/patch1.sh
+++ files/how2_ubuntu-luks2-btrfs-grub2/patch1.sh
@@ -1,1 +1,10 @@
-
+patch -b /usr/lib/partman/mount.d/70btrfs -- << '_END'
+24c24
+< 		options="${options:+$options,}subvol=@"
+---
+> 		options="${options:+$options,}subvol=@,ssd,noatime,space_cache,commit=120,compress=zstd"
+31c31
+< 		options="${options:+$options,}subvol=@home"
+---
+> 		options="${options:+$options,}subvol=@home,ssd,noatime,space_cache,commit=120,compress=zstd"
+_END
