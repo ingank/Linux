55c46cebeca74c480d433bc9e1d6313ce47547cd
Ingolf Ankert <github@ingank.net>
2021-12-04 22:24:46 +0100

    Update and rename README.build to README.sh

--- README.sh
+++ README.sh
@@ -4,28 +4,37 @@
 #
 # Dieses Skript:
 #
-# * aktualisiert über git pull das lokale Repository
-# * erzeugt automatisch eine aktuelle README.md
-# * lädt die Änderungen per git push in das remote Repository
+# aktualisiert über git pull das lokale Repository
+# erzeugt automatisch eine aktuelle README.md
+# lädt die Änderungen per git push in das remote Repository
+# wird über den cron-Dienst bspw. fünf-minütlich getriggert:
+# 5 * * * * /home/foo/Linux/README.sh
+
+a=`ping -c1 github.com 2>&1`
+if [ $? -ne 0 ]; do
+    exit
+fi
+
+a=`git fetch 2>&1`
+if [ "$a" == "" ]; do
+    exit
+fi
+
+a=`git merge 2>&1`
 
 FILE='./README.md'
 DATE=`date`
 MD='*.md'
-
 echo -e "# \`Linux\`\n" > $FILE
 echo -e "\`Das Schweizer Taschenmesser für den Linuxalltag.\`\n" >> $FILE
 echo -e "#### \`HAUPTVERZEICHNIS\`\n" >> $FILE
-
 LIST=(`ls | grep -E "${MD}" | grep -Ev README`)
-
 for NAME in "${LIST[@]}"; do
   TRAILER=$(head -1 ./$NAME | sed 's/# //g')
   echo "[\`$TRAILER\`]($NAME)<br>" >> $FILE
 done
-
 DIRS=(`tree -fid --noreport`)
 unset DIRS[0]
-
 for DIR in "${DIRS[@]}"; do
   DIR=${DIR#./}
   echo -e "\n#### \`VERZEICHNIS: $DIR\`\n" >> $FILE
@@ -35,6 +44,9 @@ for DIR in "${DIRS[@]}"; do
     echo "[\`$NAME\`]($DIR/$NAME)<br>" >> $FILE
   done
 done
-
 echo -e "\n---\n" >> $FILE
 echo "\`Aktualisiert am $DATE\`" >> $FILE
+
+a=`git add * 2>&1`
+a=`git commit -m "README.md: cron-driven update" 2>&1`
+a=`git push 2>&1`
